cecf592ef49cdb4053121da727897f52086ea487
IgorAlexCmex <ia777i888@gmail.com>
2024-11-18 13:41:32 +0300

    создание проекта игры для детей "счет -сложение чисел"

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GameCountingForChild5To10"
+include(":app")
+ 
